27424c9fddfff769bd3cbf2a747889059a937a66
HenryAI-stack <henry.ai.server@gmail.com>
2026-05-23 18:24:23 +0200

    Wrap App component with LangProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { LangProvider } from './lib/i18n.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LangProvider>
+      <App />
+    </LangProvider>
   </React.StrictMode>
 )
